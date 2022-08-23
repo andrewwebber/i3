1a06d8e10ed486a820c5ab00611cc5cb9bab2572
andrew webber (personal) <andrewvwebber@googlemail.com>
2022-08-23 12:04:55 +0200

    wip

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -2,6 +2,13 @@ local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
 saga.init_lsp_saga {
+    code_action_lightbulb = {
+        enable = true,
+        sign = true,
+        enable_in_insert = true,
+        sign_priority = 20,
+        virtual_text = true,
+    },
     server_filetype_map = {
         typescript = 'typescript'
     }
@@ -9,6 +16,7 @@ saga.init_lsp_saga {
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-n>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
