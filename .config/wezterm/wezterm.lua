71068806afa143cab10e8854803f20d84bd4e3d9
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-01-07 09:46:22 +0100

    wez

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font 'anonymice nerd font',
+    font_size = 16,
+    hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 0.8,
+    color_scheme = 'tokyonight',
+}
