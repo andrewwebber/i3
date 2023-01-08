5e73e091fd733856c68c83c5300dfe7f7e7fdf7e
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-01-08 10:39:27 +0100

    fira

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
 return {
-    font = wezterm.font 'anonymice nerd font',
+    font = wezterm.font 'fira code nerd font',
     font_size = 16,
     hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.8,
