b4404c3056796cbcecfb4f1d91066f974e0cadc2
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-01-13 13:52:57 +0100

    wip

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -1,25 +1,28 @@
-local status, saga = pcall(require, "lspsaga")
-if (not status) then return end
+-- local status, saga = pcall(require, "lspsaga")
+-- if (not status) then return end
 
-saga.init_lsp_saga {
-    code_action_lightbulb = {
-        enable = true,
-        sign = true,
-        enable_in_insert = true,
-        sign_priority = 20,
-        virtual_text = true,
-    },
-    server_filetype_map = {
-        typescript = 'typescript'
-    }
-}
+local keymap = vim.keymap.set
+
+-- saga.init_lsp_saga {
+--     code_action_lightbulb = {
+--         enable = true,
+--         sign = true,
+--         enable_in_insert = true,
+--         sign_priority = 20,
+--         virtual_text = true,
+--     },
+--     server_filetype_map = {
+--         typescript = 'typescript'
+--     }
+-- }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<C-n>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
-vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
-vim.keymap.set('n', 'ga', '<Cmd>Lspsaga code_action<CR>', opts)
+keymap('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+keymap('n', '<C-n>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
+keymap('n', '<C-i>', '<Cmd>Lspsaga show_buf_diagnostics<CR>', opts)
+keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+keymap('n', 'gD', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+keymap('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+keymap('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
+keymap('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+keymap('n', 'ga', '<Cmd>Lspsaga code_action<CR>', opts)
