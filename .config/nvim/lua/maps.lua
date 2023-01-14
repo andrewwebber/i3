0760ca28dcb7e57a797e60b0682d66d1496cbb5c
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-01-14 21:12:14 +0100

    dunst

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 keymap.set('x', '<leader>l', ':Limelight')
 keymap.set('n', '<leader>l', ':Limelight')
