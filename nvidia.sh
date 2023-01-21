a9fc30774cf92b63516a726d57e14d225447904c
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-01-21 11:06:55 +0100

    nvidia

--- nvidia.sh
+++ nvidia.sh
@@ -1,1 +1,5 @@
 yay install lib32-nvidia-utils nvidia
+sudo pacman -S lib32-mesa lib32-keyutils lib32-nvidia-utils lib32-nvidia-cg-toolkit opencl-nvidia vulkan-headers
+sudo usermod -a -G seat username
+sudo systemctl enable seatd
+sudo pacman -S nvidia nvidia-dkms
