3f8760e2f0bde4b9b62ef481481c847f7bc8f6e6
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-03-15 03:03:00 +0100

    wip

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -19,7 +19,7 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 keymap('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 keymap('n', '<C-n>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
-keymap('n', '<C-i>', '<Cmd>Lspsaga show_buf_diagnostics<CR>', opts)
+keymap('n', '<C-space>', '<Cmd>Lspsaga show_buf_diagnostics<CR>', opts)
 keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 keymap('n', 'gD', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 keymap('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
