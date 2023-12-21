872d04ec3cf6ed133b55a754a6ab139d8e398e0f
andrew webber (personal) <andrewvwebber@googlemail.com>
2023-12-21 22:07:14 +0100

    format lsp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,83 +1,83 @@
 local status, packer = pcall(require, "packer")
-if (not status) then
-    print("Packer is not installed")
-    return
+if not status then
+	print("Packer is not installed")
+	return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {
-        'svrana/neosolarized.nvim',
-        requires = { 'tjdevries/colorbuddy.nvim' }
-    }
-    use 'nvim-lualine/lualine.nvim'       -- Statusline
-    use 'nvim-lua/plenary.nvim'           -- Common utilities
-    use 'nvim-lua/popup.nvim'
-    use 'onsails/lspkind-nvim'            -- vscode-like pictograms
-    use 'hrsh7th/cmp-buffer'              -- nvim-cmp source for buffer words
-    use 'hrsh7th/cmp-nvim-lsp'            -- nvim-cmp source for neovim's built-in LSP
-    use 'hrsh7th/nvim-cmp'                -- Completion
-    use 'neovim/nvim-lspconfig'           -- LSP
-    use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-    use 'MunifTanjim/prettier.nvim'       -- Prettier plugin for Neovim's built-in LSP client
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'nvim-tree/nvim-web-devicons'
+	use("wbthomason/packer.nvim")
+	use({
+		"svrana/neosolarized.nvim",
+		requires = { "tjdevries/colorbuddy.nvim" },
+	})
+	use("nvim-lualine/lualine.nvim") -- Statusline
+	use("nvim-lua/plenary.nvim") -- Common utilities
+	use("nvim-lua/popup.nvim")
+	use("onsails/lspkind-nvim") -- vscode-like pictograms
+	use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
+	use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
+	use("hrsh7th/nvim-cmp") -- Completion
+	use("neovim/nvim-lspconfig") -- LSP
+	use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+	use("MunifTanjim/prettier.nvim") -- Prettier plugin for Neovim's built-in LSP client
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("nvim-tree/nvim-web-devicons")
 
-    use({
-        "glepnir/lspsaga.nvim",
-        branch = "main",
-        config = function()
-            require('lspsaga').setup({})
-        end,
-        requires = {
-            { "nvim-tree/nvim-web-devicons" },
-            --Please make sure you install markdown and markdown_inline parser
-            { "nvim-treesitter/nvim-treesitter" }
-        }
-    })
-    use 'L3MON4D3/LuaSnip'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        requires = { 'nvim-treesitter/nvim-treesitter' }
-    }
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-file-browser.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'windwp/nvim-ts-autotag'
-    use 'norcalli/nvim-colorizer.lua'
-    use 'folke/zen-mode.nvim'
-    use 'akinsho/nvim-bufferline.lua'
-    -- use 'github/copilot.vim'
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+			--Please make sure you install markdown and markdown_inline parser
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	})
+	use("L3MON4D3/LuaSnip")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use({
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		requires = { "nvim-treesitter/nvim-treesitter" },
+	})
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use("norcalli/nvim-colorizer.lua")
+	use("folke/zen-mode.nvim")
+	use("akinsho/nvim-bufferline.lua")
+	-- use 'github/copilot.vim'
 
-    use 'lewis6991/gitsigns.nvim'
-    use 'dinhhuy258/git.nvim' -- For git blame & browse
+	use("lewis6991/gitsigns.nvim")
+	use("dinhhuy258/git.nvim") -- For git blame & browse
 
-    use 'simrat39/rust-tools.nvim'
-    use 'tyru/open-browser.vim'
-    use 'junegunn/fzf.vim'
-    use 'tpope/vim-fugitive'
-    -- use 'bronson/vim-trailing-whitespace'
-    use 'jamessan/vim-gnupg'
-    use 'tpope/vim-commentary'
-    use 'folke/tokyonight.nvim'
-    use 'williamboman/nvim-lsp-installer'
-    use 'junegunn/limelight.vim'
-    use {
-        'saecki/crates.nvim',
-        tag = 'v0.2.1',
-        requires = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require('crates').setup()
-        end,
-    }
-    --[[use {
+	use("simrat39/rust-tools.nvim")
+	use("tyru/open-browser.vim")
+	use("junegunn/fzf.vim")
+	use("tpope/vim-fugitive")
+	-- use 'bronson/vim-trailing-whitespace'
+	use("jamessan/vim-gnupg")
+	use("tpope/vim-commentary")
+	use("folke/tokyonight.nvim")
+	use("williamboman/nvim-lsp-installer")
+	use("junegunn/limelight.vim")
+	use({
+		"saecki/crates.nvim",
+		tag = "v0.2.1",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup()
+		end,
+	})
+	--[[use {
         'kyazdani42/nvim-tree.lua',
         requires = {
             'kyazdani42/nvim-web-devicons', -- optional, for file icons
@@ -87,92 +87,96 @@ packer.startup(function(use)
             require('nvim-tree').setup()
         end,
     }]]
-    -- use 'rcarriga/nvim-notify'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-emoji' -- nvim-cmp source for neovim's built-in LSP
-    use 'rust-lang/rust.vim'
-    use {
-        "nvim-neotest/neotest",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-neotest/neotest-go",
-            "nvim-neotest/neotest-vim-test",
-            "rouge8/neotest-rust"
-        },
-        config = function()
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-go"),
-                    require("neotest-rust"),
-                    require("neotest-vim-test")
-                }
-            })
-        end
-    }
-    use 'vim-test/vim-test'
-    use {
-        "kkoomen/vim-doge",
-        run = ":call doge#install()",
-        disable = false,
-    }
-    use {
-        "iamcco/markdown-preview.nvim",
-        run = function()
-            vim.fn["mkdp#util#install"]()
-            vim.g.mkdp_browser = "/usr/bin/chromium"
-        end,
-        ft = "markdown",
-        cmd = { "MarkdownPreview" },
-        requires = { "zhaozg/vim-diagram", "aklt/plantuml-syntax" },
-    }
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    }
-    use { "ellisonleao/gruvbox.nvim" }
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
-    use({
-        "andythigpen/nvim-coverage",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("coverage").setup({
-                commands = true, -- create commands
-                highlights = {
-                    -- customize highlight groups created by the plugin
-                    covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
-                    uncovered = { fg = "#F07178" },
-                },
-                signs = {
-                    -- use your own highlight groups or text markers
-                    covered = { hl = "CoverageCovered", text = "▎" },
-                    uncovered = { hl = "CoverageUncovered", text = "▎" },
-                },
-                summary = {
-                    -- customize the summary pop-up
-                    min_coverage = 70.0, -- minimum coverage threshold (used for highlighting)
-                },
-                lang = {
-                    -- customize language specific settings
-                },
-            })
-        end,
-    })
-    use { "akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
-        require("toggleterm").setup()
-    end }
+	-- use 'rcarriga/nvim-notify'
+	use("hrsh7th/cmp-nvim-lsp-signature-help")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-emoji") -- nvim-cmp source for neovim's built-in LSP
+	use("rust-lang/rust.vim")
+	use({
+		"nvim-neotest/neotest",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-go",
+			"nvim-neotest/neotest-vim-test",
+			"rouge8/neotest-rust",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-go"),
+					require("neotest-rust"),
+					require("neotest-vim-test"),
+				},
+			})
+		end,
+	})
+	use("vim-test/vim-test")
+	use({
+		"kkoomen/vim-doge",
+		run = ":call doge#install()",
+		disable = false,
+	})
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+			vim.g.mkdp_browser = "/usr/bin/chromium"
+		end,
+		ft = "markdown",
+		cmd = { "MarkdownPreview" },
+		requires = { "zhaozg/vim-diagram", "aklt/plantuml-syntax" },
+	})
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+	use({ "ellisonleao/gruvbox.nvim" })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
+	use({
+		"andythigpen/nvim-coverage",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("coverage").setup({
+				commands = true, -- create commands
+				highlights = {
+					-- customize highlight groups created by the plugin
+					covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+					uncovered = { fg = "#F07178" },
+				},
+				signs = {
+					-- use your own highlight groups or text markers
+					covered = { hl = "CoverageCovered", text = "▎" },
+					uncovered = { hl = "CoverageUncovered", text = "▎" },
+				},
+				summary = {
+					-- customize the summary pop-up
+					min_coverage = 70.0, -- minimum coverage threshold (used for highlighting)
+				},
+				lang = {
+					-- customize language specific settings
+				},
+			})
+		end,
+	})
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "v2.*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
 end)
--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -1,67 +1,75 @@
 --vim.lsp.set_log_level("debug")
 
 local status, nvim_lsp = pcall(require, "lspconfig")
-if (not status) then return end
+if not status then
+	return
+end
 
-local protocol = require('vim.lsp.protocol')
-local configs = require('lspconfig/configs')
+local protocol = require("vim.lsp.protocol")
+local configs = require("lspconfig/configs")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    --Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	--Enable completion triggered by <c-x><c-o>
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    local opts = { noremap = true, silent = true }
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    -- buf_set_keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	-- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	-- buf_set_keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
 
-    -- formatting
-    if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("Format", { clear = true }),
-            buffer = bufnr,
-            callback = function() vim.lsp.buf.format() end
-        })
-    end
+	-- formatting
+	if client.server_capabilities.documentFormattingProvider then
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = vim.api.nvim_create_augroup("Format", { clear = true }),
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
+	end
 end
 
 protocol.CompletionItemKind = {
-    '', -- Text
-    '', -- Method
-    '', -- Function
-    '', -- Constructor
-    '', -- Field
-    '', -- Variable
-    '', -- Class
-    'ﰮ', -- Interface
-    '', -- Module
-    '', -- Property
-    '', -- Unit
-    '', -- Value
-    '', -- Enum
-    '', -- Keyword
-    '﬌', -- Snippet
-    '', -- Color
-    '', -- File
-    '', -- Reference
-    '', -- Folder
-    '', -- EnumMember
-    '', -- Constant
-    '', -- Struct
-    '', -- Event
-    'ﬦ', -- Operator
-    '', -- TypeParameter
+	"", -- Text
+	"", -- Method
+	"", -- Function
+	"", -- Constructor
+	"", -- Field
+	"", -- Variable
+	"", -- Class
+	"ﰮ", -- Interface
+	"", -- Module
+	"", -- Property
+	"", -- Unit
+	"", -- Value
+	"", -- Enum
+	"", -- Keyword
+	"﬌", -- Snippet
+	"", -- Color
+	"", -- File
+	"", -- Reference
+	"", -- Folder
+	"", -- EnumMember
+	"", -- Constant
+	"", -- Struct
+	"", -- Event
+	"ﬦ", -- Operator
+	"", -- TypeParameter
 }
 
 -- Set up completion using nvim_cmp with LSP source
@@ -69,29 +77,28 @@ protocol.CompletionItemKind = {
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(
 --     vim.lsp.protocol.make_client_capabilities()
 -- )
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-nvim_lsp.lua_ls.setup {
-    on_attach = on_attach
-}
+nvim_lsp.lua_ls.setup({
+	on_attach = on_attach,
+})
 
-nvim_lsp.flow.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+nvim_lsp.flow.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-nvim_lsp.tailwindcss.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
+nvim_lsp.tailwindcss.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-}
-
-nvim_lsp.tsserver.setup {
-    on_attach = on_attach,
-    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities
-}
+nvim_lsp.tsserver.setup({
+	on_attach = on_attach,
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+	cmd = { "typescript-language-server", "--stdio" },
+	capabilities = capabilities,
+})
 
 -- nvim_lsp.sumneko_lua.setup {
 --     on_attach = on_attach,
@@ -116,42 +123,42 @@ nvim_lsp.tsserver.setup {
 -- })
 
 nvim_lsp.jedi_language_server.setup({
-    on_attach = on_attach
+	on_attach = on_attach,
 })
 nvim_lsp.pyright.setup({
-    on_attach = on_attach,
-    useLibraryCodeForTypes = true
+	on_attach = on_attach,
+	useLibraryCodeForTypes = true,
 })
 
 if not configs.ruff_lsp then
-    configs.ruff_lsp = {
-        default_config = {
-            cmd = { 'ruff-lsp' },
-            filetypes = { 'python' },
-            root_dir = require('lspconfig').util.find_git_ancestor,
-            init_options = {
-                settings = {
-                    args = {}
-                }
-            }
-        }
-    }
+	configs.ruff_lsp = {
+		default_config = {
+			cmd = { "ruff-lsp" },
+			filetypes = { "python" },
+			root_dir = require("lspconfig").util.find_git_ancestor,
+			init_options = {
+				settings = {
+					args = {},
+				},
+			},
+		},
+	}
 end
 
 nvim_lsp.ruff_lsp.setup({
-    on_attach = on_attach,
-    {
-        default_config = {
-            cmd = { 'ruff-lsp' },
-            filetypes = { 'python' },
-            root_dir = require('lspconfig').util.find_git_ancestor,
-            init_options = {
-                settings = {
-                    args = {}
-                }
-            }
-        }
-    }
+	on_attach = on_attach,
+	{
+		default_config = {
+			cmd = { "ruff-lsp" },
+			filetypes = { "python" },
+			root_dir = require("lspconfig").util.find_git_ancestor,
+			init_options = {
+				settings = {
+					args = {},
+				},
+			},
+		},
+	},
 })
 -- nvim_lsp.pylsp.setup({
 --     settings = {
@@ -172,106 +179,103 @@ nvim_lsp.ruff_lsp.setup({
 --     }
 -- })
 
-local util = require "lspconfig/util"
+local util = require("lspconfig/util")
 
-nvim_lsp.gopls.setup {
-    cmd = { "gopls", "serve" },
-    filetypes = { "go", "gomod" },
-    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-            },
-            staticcheck = true,
-        },
-    },
-}
+nvim_lsp.gopls.setup({
+	cmd = { "gopls", "serve" },
+	filetypes = { "go", "gomod" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
+})
 
 if not configs.golangcilsp then
-    configs.golangcilsp = {
-        default_config = {
-            cmd = { 'golangci-lint-langserver' },
-            root_dir = nvim_lsp.util.root_pattern('.git', 'go.mod'),
-            init_options = {
-                command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json" },
-            }
-        },
-    }
+	configs.golangcilsp = {
+		default_config = {
+			cmd = { "golangci-lint-langserver" },
+			root_dir = nvim_lsp.util.root_pattern(".git", "go.mod"),
+			init_options = {
+				command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json" },
+			},
+		},
+	}
 end
 
-nvim_lsp.golangci_lint_ls.setup {
-    filetypes = { 'go', 'gomod' }
-}
+nvim_lsp.golangci_lint_ls.setup({
+	filetypes = { "go", "gomod" },
+})
 
 local opts = {
-    tools = {
-        autoSetHints = true,
-        inlay_hints = {
-            show_parameter_hints = true,
-            other_hints_prefix = "\194\187 ",
-            only_current_line_autocmd = "CursorHold",
-            only_current_line = false,
-            right_align_padding = 7,
-            parameter_hints_prefix = "< ",
-            right_align = false,
-            highlight = "RustInlayHint",
-            max_len_align = false,
-            max_len_align_padding = 1
-        }
-    },
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-            ['rust-analyzer'] = {
-                cargo = {
-                    checkOnSave = {
-                        enable = true,
-                        command = "clippy",
-                        extraArgs = "--tests -- -Dwarnings -A deprecated",
-                        allFeatures = false,
-                        overrideCommand = {
-                            "cargo",
-                            "clippy",
-                            "--tests -- -Dwarnings -A deprecated"
-                        }
-                    }
-                }
-            }
-        }
-    }
+	tools = {
+		autoSetHints = true,
+		inlay_hints = {
+			show_parameter_hints = true,
+			other_hints_prefix = "\194\187 ",
+			only_current_line_autocmd = "CursorHold",
+			only_current_line = false,
+			right_align_padding = 7,
+			parameter_hints_prefix = "< ",
+			right_align = false,
+			highlight = "RustInlayHint",
+			max_len_align = false,
+			max_len_align_padding = 1,
+		},
+	},
+	server = {
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = {
+			["rust-analyzer"] = {
+				cargo = {
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+						extraArgs = "--tests -- -Dwarnings -A deprecated",
+						allFeatures = false,
+						overrideCommand = {
+							"cargo",
+							"clippy",
+							"--tests -- -Dwarnings -A deprecated",
+						},
+					},
+				},
+			},
+		},
+	},
 }
 
+require("rust-tools").setup(opts)
 
-require('rust-tools').setup(opts)
+require("nvim-lsp-installer").setup({})
 
-require("nvim-lsp-installer").setup {}
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
-        severity_sort = true,
-    }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	update_in_insert = false,
+	virtual_text = { spacing = 4, prefix = "●" },
+	severity_sort = true,
+})
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-    virtual_text = {
-        prefix = '●'
-    },
-    update_in_insert = true,
-    float = {
-        source = "always", -- Or "if_many"
-    },
+	virtual_text = {
+		prefix = "●",
+	},
+	update_in_insert = true,
+	float = {
+		source = "always", -- Or "if_many"
+	},
 })
 
 vim.cmd([[
@@ -279,8 +283,8 @@ set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
-require 'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = true
-    },
-}
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+	},
+})
