ad11f7108152c5ffc7b7dcae7badd21a5b2a074f
andrew webber (personal) <andrewvwebber@googlemail.com>
2024-01-03 11:39:38 +0100

    fish gpg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,12 @@ starship init fish | source
 source ~/projects/i3/.config/fish/fish_greeting.fish
 source ~/projects/i3/.config/fish/conf.d/abbr.fish
 source ~/projects/i3/.config/fish/functions.fish
+
+set -gx EDITOR /usr/bin/vim
+set -gx VISUAL /usr/bin/vim
+
+set -gx GPG_TTY (tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
+set -e SSH_AGENT_PID
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
