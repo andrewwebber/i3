fe7fd9d54efd5abb08c015092b9afd2b595b6637
andrew webber (personal) <andrewvwebber@googlemail.com>
2024-02-01 07:12:45 +0100

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,8 @@ set -e SSH_AGENT_PID
 set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 
+source /opt/google-cloud-cli/path.fish.inc
+
 set PATH ~/.local/bin/ $PATH
 
 if test (tty) = /dev/tty1
