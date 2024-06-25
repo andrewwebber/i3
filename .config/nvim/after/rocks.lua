90715a792904c8bfa2f5f3ed5b8a01bf07038f31
andrew webber (personal) <andrewvwebber@googlemail.com>
2024-06-25 10:13:58 +0200

    wip

--- .config/nvim/after/rocks.lua
+++ .config/nvim/after/rocks.lua
@@ -1,7 +1,10 @@
+vim.opt.runtimepath:prepend("~/.local/parsers")
 vim.g.rocks_nvim = {
 	-- rocks.nvim config
 	treesitter = {
+
 		auto_highlight = "all",
-		auto_install = "true",
+		auto_install = true,
+		parser_install_dir = "~/.local/parsers",
 	},
 }
