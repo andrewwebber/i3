6525ae2cc0571b84df053ddb1edd1d856c290ab5
andrew webber (personal) <andrewvwebber@googlemail.com>
2024-07-03 19:47:03 +0200

    neovim sync

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -45,31 +45,17 @@ local api = vim.api
 -- Rust
 local rustUtil = api.nvim_create_augroup("RustUtil", { clear = true })
 api.nvim_create_autocmd("FileType", {
-    pattern = { "rust" },
-    command = "nmap gb :Cargo clippy --tests -- -Dwarnings -A deprecated<CR>",
-    group = rustUtil,
+  pattern = { "rust" },
+  command = "nmap gb :Cargo clippy --tests -- -Dwarnings -A deprecated<CR>",
+  group = rustUtil,
 })
 api.nvim_create_autocmd("FileType", {
-    pattern = { "rust" },
-    command = "nmap gB :Cargo clippy --tests --all-features -- -Dwarnings -A deprecated<CR>",
-    group = rustUtil,
+  pattern = { "rust" },
+  command = "nmap gB :Cargo clippy --tests --all-features -- -Dwarnings -A deprecated<CR>",
+  group = rustUtil,
 })
 api.nvim_create_autocmd("FileType", {
-    pattern = { "rust" },
-    command =
-    'nmap <leader>t :lua require("neotest").run.run({vim.fn.expand("%"), env = {RUSTFLAGS="-C instrument-coverage"}})<CR>',
-    group = rustUtil,
-})
-
--- Golang
-local goUtil = api.nvim_create_augroup("GolangUtil", { clear = true })
-api.nvim_create_autocmd("FileType", {
-    pattern = { "go" },
-    command = ":Coverage",
-    group = goUtil,
-})
-api.nvim_create_autocmd("FileType", {
-    pattern = { "go" },
-    command = "nmap <leader>t :TestNearest -strategy=neovim --coverprofile=coverage.out<CR>| :Coverage",
-    group = rustUtil,
+  pattern = { "rust" },
+  command = 'nmap <leader>t :lua require("neotest").run.run({vim.fn.expand("%"), env = {RUSTFLAGS="-C instrument-coverage"}})<CR>',
+  group = rustUtil,
 })
