4409b839068a51ad9c2d4db2158ff3077a3afd46
andrew webber (personal) <andrewvwebber@googlemail.com>
2024-07-22 18:25:35 +0200

    yaml prettier

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -134,7 +134,7 @@ return {
         ["html"] = { "prettier" },
         ["json"] = { "prettier" },
         ["jsonc"] = { "prettier" },
-        ["yaml"] = { "prettier" },
+        -- ["yaml"] = { "prettier" },
         ["markdown"] = { "prettier" },
         ["markdown.mdx"] = { "prettier" },
         ["graphql"] = { "prettier" },
