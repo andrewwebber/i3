6c5ea6b85f5edc11ca99464cf09969233d978958
andrew webber (personal) <andrewvwebber@googlemail.com>
2025-01-28 22:13:55 +0100

    ollama

---
+++ .config/nvim/after/plugin/ollama.lua
@@ -0,0 +1,4 @@
+require("gen").prompts["Analysis"] = {
+  prompt = "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.\n\n$input\n\nQuestion: $text\nHelpful Answer:",
+  replace = true,
+}
