e59284dfcc2ec6a87433c1d6eb2824e28195bb69
andrew webber (personal) <andrewvwebber@googlemail.com>
2025-03-24 10:27:52 +0100

    wip: switch to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 return {
-    font = wezterm.font 'fira code nerd font',
-    font_size = 16,
-    hide_tab_bar_if_only_one_tab = true,
-    window_background_opacity = 0.7,
-    color_scheme = 'tokyonight',
-    enable_wayland = true,
+	font = wezterm.font("fira code nerd font"),
+	font_size = 14,
+	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.8,
+	color_scheme = "tokyonight",
+	enable_wayland = true,
 }
