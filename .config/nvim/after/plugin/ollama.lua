53bf1deb7b4328a509fd9a74744df0c23e9c8bd7
andrew webber (personal) <andrewvwebber@googlemail.com>
2025-05-23 14:39:24 +0200

    wip

--- .config/nvim/after/plugin/ollama.lua
+++ .config/nvim/after/plugin/ollama.lua
@@ -1,7 +1,7 @@
 require("gen").prompts["Analysis"] = {
   prompt = "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.\n\n$input\n\nQuestion: $text\nHelpful Answer:",
   replace = false,
-  model = "deepseek-coder:8b",
+  model = "llama3.1:8b",
 }
 
 require("gen").prompts["Coder_Review_Code"] = {
