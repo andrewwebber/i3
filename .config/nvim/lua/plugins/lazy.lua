36a0b68d0505153092b7a5348bb2bb3415fb5f2c
andrew webber (personal) <andrewvwebber@googlemail.com>
2025-12-13 00:47:37 +0100

    feat: base64

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -2,6 +2,13 @@ return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "jamessan/vim-gnupg",
   {
+    "ovk/endec.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- Override default configuration here
+    },
+  },
+  {
     "echasnovski/mini.diff",
     config = function()
       local diff = require("mini.diff")
@@ -55,18 +62,18 @@ return {
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "llama3.2:3b",             -- The default model to use.
-      quit_map = "q",                    -- set keymap to close the response window
-      retry_map = "<c-r>",               -- set keymap to re-send the current prompt
-      accept_map = "<c-cr>",             -- set keymap to replace the previous selection with the last result
-      host = "localhost",                -- The host running the Ollama service.
-      port = "11434",                    -- The port on which the Ollama service is listening.
+      model = "llama3.2:3b", -- The default model to use.
+      quit_map = "q", -- set keymap to close the response window
+      retry_map = "<c-r>", -- set keymap to re-send the current prompt
+      accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+      host = "localhost", -- The host running the Ollama service.
+      port = "11434", -- The port on which the Ollama service is listening.
       display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split".
-      show_prompt = true,                -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-      show_model = true,                 -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false,             -- Never closes the window automatically.
-      file = false,                      -- Write the payload to a temporary file to keep the command short.
-      hidden = false,                    -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+      show_prompt = true, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      file = false, -- Write the payload to a temporary file to keep the command short.
+      hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
       init = function(options)
         -- pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
       end,
@@ -74,10 +81,10 @@ return {
       command = function(options)
         local body = { model = options.model, stream = true }
         return "curl --silent --no-buffer -X POST http://"
-            .. options.host
-            .. ":"
-            .. options.port
-            .. "/api/chat -d $body"
+          .. options.host
+          .. ":"
+          .. options.port
+          .. "/api/chat -d $body"
       end,
       -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
       -- This can also be a command string.
@@ -85,7 +92,7 @@ return {
       -- (context property is optional).
       -- list_models = '<omitted lua function>', -- Retrieves a list of model names
       result_filetype = "markdown", -- Configure filetype of the result buffer
-      debug = false,                -- Prints errors and the command which is run.
+      debug = false, -- Prints errors and the command which is run.
     },
   },
   {
@@ -226,21 +233,21 @@ return {
       require("which-key").setup()
 
       require("which-key").add({
-        { "<leader>c",  group = "[C]ode" },
+        { "<leader>c", group = "[C]ode" },
         { "<leader>c_", hidden = true },
-        { "<leader>d",  group = "[D]ocument" },
+        { "<leader>d", group = "[D]ocument" },
         { "<leader>d_", hidden = true },
-        { "<leader>h",  group = "Git [H]unk" },
+        { "<leader>h", group = "Git [H]unk" },
         { "<leader>h_", hidden = true },
-        { "<leader>r",  group = "[R]ename" },
+        { "<leader>r", group = "[R]ename" },
         { "<leader>r_", hidden = true },
-        { "<leader>s",  group = "[S]earch" },
+        { "<leader>s", group = "[S]earch" },
         { "<leader>s_", hidden = true },
-        { "<leader>t",  group = "[T]oggle" },
+        { "<leader>t", group = "[T]oggle" },
         { "<leader>t_", hidden = true },
-        { "<leader>w",  group = "[W]orkspace" },
+        { "<leader>w", group = "[W]orkspace" },
         { "<leader>w_", hidden = true },
-        { "<leader>x",  group = "[T]rouble" },
+        { "<leader>x", group = "[T]rouble" },
         { "<leader>x_", hidden = true },
       })
       -- Document existing key chains
