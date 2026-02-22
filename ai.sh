0d8793ffeb4625c60b248e6b078a0b047d2995f8
andrew webber (personal) <andrewvwebber@googlemail.com>
2026-02-22 22:17:06 +0100

    feat: ai

--- ai.sh
+++ ai.sh
@@ -1,9 +1,11 @@
 #! /bin/sh -xe
-llama-server  -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M\
+# llama-server  -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M\
+
+llama-server  -hf unsloth/Qwen3-Coder-Next-GGUF:MXFP4_MOE\
     --port 8012 \
     --jinja \
     -ngl 32 \
-    -c 32000 \
+    -c 132000 \
     --flash-attn on  \
     -b 512  \
     --temp 1.1 \
