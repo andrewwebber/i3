9c6e1b11db2da967f21421ba8a0e45999653aa29
andrew webber (personal) <andrewvwebber@googlemail.com>
2026-02-25 20:47:43 +0100

    feat: ai

--- ai.sh
+++ ai.sh
@@ -1,20 +1,4 @@
 #! /bin/sh -e
-function thinking_full(){
-  llama-server  -hf unsloth/Qwen3-Coder-Next-GGUF:MXFP4_MOE\
-      --port 8012 \
-      --jinja \
-      -ngl 32 \
-      -c 132000 \
-      --flash-attn on  \
-      -b 512  \
-      --temp 1.1 \
-      --top-k 40 \
-      --top-p 0.95 \
-      --repeat-penalty 1.05  \
-      -cmoe \
-      -fit on
-}
-
 function thinking(){
   llama-server  -hf unsloth/Qwen3.5-35B-A3B-GGUF:Q4_K_M\
       --port 8012 \
@@ -32,12 +16,11 @@ function thinking(){
       # --chat-template-kwargs "{\"enable_thinking\": false}"
 }
 
-function fast(){
-    # unsloth/Qwen3-Coder-Next-GGUF:MXFP4_MOE
-  llama-server  -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M\
+function coding(){
+  llama-server  -hf unsloth/Qwen3-Coder-Next-GGUF:MXFP4_MOE\
       --port 8012 \
       --jinja \
-      -ngl 99 \
+      -ngl 32 \
       -c 132000 \
       --flash-attn on  \
       -b 512  \
@@ -67,9 +50,8 @@ opt=$1
 case $opt
 in
     s) small ;;
-    f) fast ;;
-    p) thinking ;;
-    t) thinking_full ;;
+    c) coding ;;
+    t) thinking ;;
     *) echo "Nothing to do"
        exit ;;
 esac
