2896e0dc6b56101f56be0f0c7738a42b09d26514
andrew webber (personal) <andrewvwebber@googlemail.com>
2026-02-25 09:19:44 +0100

    feat: ai

--- ai.sh
+++ ai.sh
@@ -48,9 +48,26 @@ function fast(){
       --repeat-penalty 1.05 
 }
 
+function small(){
+  llama-server  -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M\
+      --port 8012 \
+      --jinja \
+      -ngl 99 \
+      -c 132000 \
+      --flash-attn on  \
+      -b 512  \
+      --temp 1.1 \
+      --top-k 40 \
+      --top-p 0.95 \
+      --repeat-penalty 1.05 \
+      -cmoe \
+      -fit on
+}
+
 opt=$1
 case $opt
 in
+    s) small ;;
     f) fast ;;
     p) thinking ;;
     t) thinking_full ;;
